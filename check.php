12d65820e635d42f96d5154f0b9755981c92c74b
cfaulk <faulkerson@gmail.com>
2016-03-08 01:42:02 -0600

    some_message

---
+++ check.php
@@ -0,0 +1,43 @@
+<?php
+/*---CREATED BY Corey Faulkerson. Find me at dudermang.com ---*/
+function checkserve() {
+//set ip address of the developement server
+$dserver = '104.131.219.41';
+//set ip address of the production server
+$pserver = '104.236.161.234';
+
+if($_SERVER['SERVER_ADDR']==$dserver)
+	{
+		#this is the Developement server
+		$serv= "Developement";
+ 	} 
+else
+{
+if($_SERVER['SERVER_ADDR']==$pserver)
+	{
+		#this is the production server
+		$serv="Production";
+	}
+}
+
+//displays "Developement" or "Production" based on the servers IP address
+echo $serv;
+}
+
+//to call this funtion just include the file in your code and call the function like seen below.
+echo checkserve();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
